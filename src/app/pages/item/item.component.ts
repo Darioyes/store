495eefd280529d58ab68e34423bf0e4ef7b16f59
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2023-04-24 21:03:43 -0500

    buscar por producto

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { productoDescripcion } from 'src/app/interfaces/producto-descripcion.interface';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
+
+  private route = inject(ActivatedRoute);
+  public productoService = inject(ProductosService);
+
+  producto: productoDescripcion={}; 
+  id:string='';
+
+  constructor(
+    // private route: ActivatedRoute
+    //public productoService: ProductosService
+  ){}
+
+  ngOnInit(){
+    this.route.params.subscribe( parametros =>{
+      //console.log(parametros['id']);
+
+      this.productoService.getProducto(parametros['id'])
+                          .subscribe( producto =>{
+        this.id = parametros['id'];
+        this.producto = producto;
+        // console.log(producto);
+      });
+    });
+  }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -24,4 +24,8 @@ export class ProductosService {
 
     });
   }
+
+  getProducto( id:string){
+    return this.http.get(`https://angular-html-ym-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
 }
